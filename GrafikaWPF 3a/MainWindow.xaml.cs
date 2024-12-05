227746761a66f3329b4ae37d16299f99460299b1
markup54 <marzenakupka@tlen.pl>
2024-12-05 08:04:46 +0100

    Dodaj pliki projektów.

---
+++ GrafikaWPF 3a/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GrafikaWPF_3a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            image_gory.Source = new BitmapImage(new Uri("rys2.jpg",UriKind.Relative));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            List<string> listObrazkow = new List<string>();
+            listObrazkow.Add("rys1.jpg");
+            listObrazkow.Add("rys2.jpg");
+            listObrazkow.Add("rys3.jpg");
+            listObrazkow.Add("rys4.jpg");
+            Random random = new Random();
+            int indeks = random.Next(listObrazkow.Count);
+            image_gory.Source = new BitmapImage(new Uri(listObrazkow[indeks], UriKind.Relative));
+        }
+    }
+}
---
+++ GrafikaWPF 3a/WindowKaruzela.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GrafikaWPF_3a
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WindowKaruzela.xaml
+    /// </summary>
+    /// 
+    public partial class WindowKaruzela : Window
+    {
+        public int Licznik { get; set; }
+        public List<string> obrazki = new List<string>();
+        public WindowKaruzela()
+        {
+            InitializeComponent();
+            obrazki.Add("rys1.jpg");
+            obrazki.Add("rys2.jpg");
+            obrazki.Add("rys3.jpg");
+            obrazki.Add("rys4.jpg");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Licznik--;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Licznik++;
+        }
+    }
+}
